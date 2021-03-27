b9920074aa0cfb2067b39e8c13db718108f020f0
sebastian <sebastian.calleu@gmail.com>
2021-03-26 22:25:58 -0500

    adding modifications on constructor

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,20 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if('updated_at' in kwargs.keys()):
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            if('created_at' in kwargs.keys()):
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            if('__class__' in kwargs.keys()):
+                del kwargs['__class__']
+            else:
+                from models import storage
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                storage.new(self)
             self.__dict__.update(kwargs)
 
     def __str__(self):
