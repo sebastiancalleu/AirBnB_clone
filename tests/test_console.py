82040e2502a2a6e6ef5fa7ac12fa8c28963eec90
sebastian <sebastian.calleu@gmail.com>
2021-03-28 22:31:00 -0500

    fix check

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,5 +9,5 @@ class test_console(unittest.TestCase):
     def test_pep8(self):
         """method for test pep8 on console.py"""
         pep8check = pep8.StyleGuide(quite=True)
-        check = pep8style.check_files(['console.py'])
+        check = pep8check.check_files(['console.py'])
         self.assertEqual(check.total_errors, 0, "style error go fix it."
