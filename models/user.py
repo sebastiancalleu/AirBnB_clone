66bf0cc49565f3ba175b68e8ef6e44cfeb0e1156
sebastian <sebastian.calleu@gmail.com>
2021-03-29 12:41:11 -0500

    modifying option nullable on first and last name.

--- models/user.py
+++ models/user.py
@@ -12,8 +12,8 @@ class User(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(180), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(180), nullable=True)
         places = relationship('Place', cascade="delete")
         reviews = relationship('Review', cascade="delete")
     else:
