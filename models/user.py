93f3d3462e3ae36ac9e7d953302af81d054af2fa
sebastian <sebastian.calleu@gmail.com>
2021-03-28 21:39:41 -0500

    removing unnecesary import from sqlalchemy

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 import os
