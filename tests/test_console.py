0b11ac49007eb78ba90ed8ea19b2179b81630c34
sebastian <sebastian.calleu@gmail.com>
2021-03-28 22:31:52 -0500

    fixing option for styleguide

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,6 +8,6 @@ class test_console(unittest.TestCase):
 
     def test_pep8(self):
         """method for test pep8 on console.py"""
-        pep8check = pep8.StyleGuide(quite=True)
+        pep8check = pep8.StyleGuide(quiet=True)
         check = pep8check.check_files(['console.py'])
         self.assertEqual(check.total_errors, 0, "style error go fix it."
