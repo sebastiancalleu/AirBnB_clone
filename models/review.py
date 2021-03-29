83abdf09fb0bb051a52a2121d4ee1de5d66359d5
sebastian <sebastian.calleu@gmail.com>
2021-03-29 13:23:03 -0500

    comenting out relationships.

--- models/review.py
+++ models/review.py
@@ -13,8 +13,8 @@ class Review(BaseModel, Base):
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        user = relationship('User')
-        place = relationship('Place')
+        #user = relationship('User')
+        #place = relationship('Place')
     else:
         place_id = ""
         user_id = ""
