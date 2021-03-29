04d06c3ce3d1036275a02616f46f7d3223ab1172
sebastian <sebastian.calleu@gmail.com>
2021-03-28 22:25:23 -0500

    adding the test file for first time.

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Test suit"""
+import unittest
+import pep8
+
+class test_console(unittest.TestCase):
+    """unittest class for console"""
+
+    def test_pep8(self):
+        """method for test pep8 on console.py"""
+        pep8check = pep8.StyleGuide(quite=True)
+        check = pep8style.check_files(['console.py'])
+        self.assertEqual(check.tota_errors, 0, "style error go fix it."
