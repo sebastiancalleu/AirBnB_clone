6d100a50714510c16629aec342071bee94684ceb
sebastian <sebastian.calleu@gmail.com>
2021-04-15 14:04:40 -0500

    adding files for first time

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from fabric.api import local
+import datetime
+import os
+
+def do_pack():
+    str1 = (str(datetime.datetime.now()).split("."))[0]
+    str1 = str1.replace("-", "").replace(":", "").replace(" ", "")
+    command1 = local("mkdir -p versions")
+    if command1.failed:
+        return(None)
+    path = "versions/web_static_{}.tgz".format(str1)
+    print("Packing web_static to {}".format(path))
+    command2 = local("tar -cvzf {} web_static".format(path))
+    if command2.failed:
+        return(None)
+    print("web_static packed: {} -> {}Bytes".format(path, os.path.getsize(path)))
+    return("versions/web_static_{}.tgz".format(str1))
