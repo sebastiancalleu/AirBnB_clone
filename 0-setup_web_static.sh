1f8c5e69bcc2946a09558232832a0acdb2d34aac
sebastian <sebastian.calleu@gmail.com>
2021-04-15 14:50:32 -0500

    adding back duplication check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,8 @@ touch /data/web_static/releases/test/index.html
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-#if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default
-#then
-sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tautoindex off;\n\t} " /etc/nginx/sites-available/default
-#fi
+if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default
+then
+	sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tautoindex off;\n\t} " /etc/nginx/sites-available/default
+fi
 service nginx restart
