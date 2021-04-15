8bc339f494a05d6483851185e8ca465b15f9d251
sebastian <sebastian.calleu@gmail.com>
2021-04-15 14:44:47 -0500

    adding autoindex off

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default
 then
-    sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t} " /etc/nginx/sites-available/default
+    sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tautoindex off;\n\t} " /etc/nginx/sites-available/default
 fi
 service nginx restart
