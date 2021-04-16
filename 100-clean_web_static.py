4806c925e8026af836ddeff351889522387499fe
sebastian <sebastian.calleu@gmail.com>
2021-04-16 18:54:19 -0500

    adding file for first time

---
+++ 100-clean_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from fabric.api import local, run, env
+import datetime
+import os
+
+
+def do_clean(number=0):
+    if int(number) == 0:
+        keepfiles = 2
+    else:
+        keepfiles = int(number) + 1
+    local("ls -t versions/ | tail -n +{} | xargs -I _ rm versions/_")
+    run("ls -t /data/web_static/releases/ | tail -n +{} | xargs -I _ rm /data/web_static/releases/_".format(keepfiles))
