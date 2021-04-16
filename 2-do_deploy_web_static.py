519e460053fbb37dea0bdcd0105b708baa41ac28
sebastian <sebastian.calleu@gmail.com>
2021-04-15 22:32:58 -0500

    modifying the initial conditional

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,29 +9,28 @@ env.hosts = ['35.196.217.143', '3.85.208.251']
 
 def do_deploy(archive_path):
     """ function to deploy """
-    if path.isfile(archive_path):
-        try:
-            filename = archive_path.split("/")[1]
-            filenamewe = filename.replace(".tgz", "")
-            put('{}'.format(archive_path), '/tmp/')
-            run('mkdir -p /data/web_static/releases/{}'.format(filenamewe))
-            run('tar -zxf /tmp/{} -C /data/web_static/releases/{}'
-                .format(filename, filenamewe))
-            run('rm /tmp/{}'.format(filename))
-            route1 = ("/data/web_static/releases/{}/web_static/*"
-                      .format(filenamewe))
-            route2 = ("/data/web_static/releases/{}/"
-                      .format(filenamewe))
-            run('mv {} {}'
-                .format(route1, route2))
-            run('rm -rf /data/web_static/releases/{}/web_static'
-                .format(filenamewe))
-            run('rm -rf /data/web_static/current')
-            run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-                .format(filenamewe))
-            print("New version deployed!")
-            return(True)
-        except:
-            return(False)
-    else:
+    if path.isfile(archive_path) is False:
+        return(False)
+    try:
+        filename = archive_path.split("/")[1]
+        filenamewe = filename.replace(".tgz", "")
+        put('{}'.format(archive_path), '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(filenamewe))
+        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}'
+            .format(filename, filenamewe))
+        run('rm /tmp/{}'.format(filename))
+        route1 = ("/data/web_static/releases/{}/web_static/*"
+                  .format(filenamewe))
+        route2 = ("/data/web_static/releases/{}/"
+                  .format(filenamewe))
+        run('mv {} {}'
+            .format(route1, route2))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(filenamewe))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(filenamewe))
+        print("New version deployed!")
+        return(True)
+    except:
         return(False)
