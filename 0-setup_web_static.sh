dd76d4c770c2a3dfbc462796441f517f25194110
sebastian <sebastian.calleu@gmail.com>
2021-04-15 23:12:38 -0500

    adding sudo all

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,12 +6,13 @@ then
     sudo apt-get install -y nginx
     sudo service nginx start
 fi
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default
-then
-	sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tautoindex off;\n\t} " /etc/nginx/sites-available/default
-fi
+sudo chown -R ubuntu:ubuntu /data/
+#if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default
+#then
+sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tautoindex off;\n\t} " /etc/nginx/sites-available/default
+#fi
 sudo service nginx restart
