d4347a5b12663c1d5239e2890a56b2f43e7bf2ea
sebastian <sebastian.calleu@gmail.com>
2021-04-15 22:44:31 -0500

    modifying structure

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ script to deploy a file to servers """
 from fabric.api import *
-from os import path
+from os.path import isfile
 import datetime
 
 env.hosts = ['35.196.217.143', '3.85.208.251']
@@ -9,28 +9,26 @@ env.hosts = ['35.196.217.143', '3.85.208.251']
 
 def do_deploy(archive_path):
     """ function to deploy """
-    if path.isfile(archive_path) is False:
+    if isfile(archive_path) is False:
+        print("wtf")
         return(False)
-    try:
-        filename = archive_path.split("/")[1]
-        filenamewe = filename.replace(".tgz", "")
-        put('{}'.format(archive_path), '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}'.format(filenamewe))
-        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}'
-            .format(filename, filenamewe))
-        run('rm /tmp/{}'.format(filename))
-        route1 = ("/data/web_static/releases/{}/web_static/*"
-                  .format(filenamewe))
-        route2 = ("/data/web_static/releases/{}/"
-                  .format(filenamewe))
-        run('mv {} {}'
-            .format(route1, route2))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filenamewe))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(filenamewe))
-        print("New version deployed!")
-        return(True)
-    except:
-        return(False)
+    filename = archive_path.split("/")[1]
+    filenamewe = filename.replace(".tgz", "")
+    put(archive_path, '/tmp/')
+    run('mkdir -p /data/web_static/releases/{}'.format(filenamewe))
+    run('tar -zxf /tmp/{} -C /data/web_static/releases/{}'
+        .format(filename, filenamewe))
+    run('rm /tmp/{}'.format(filename))
+    route1 = ("/data/web_static/releases/{}/web_static/*"
+              .format(filenamewe))
+    route2 = ("/data/web_static/releases/{}/"
+              .format(filenamewe))
+    run('mv {} {}'
+        .format(route1, route2))
+    run('rm -rf /data/web_static/releases/{}/web_static'
+        .format(filenamewe))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        .format(filenamewe))
+    print("New version deployed!")
+    return(True)
