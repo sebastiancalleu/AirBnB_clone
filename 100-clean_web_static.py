5e5b9b591d22ab6cfc3ac9fead84130f8001ed7c
sebastian <sebastian.calleu@gmail.com>
2021-04-16 21:39:30 -0500

    fixing commands

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,6 +4,8 @@ from fabric.api import local, run, env
 import datetime
 import os
 
+env.hosts = ['35.196.217.143', '3.85.208.251']
+
 
 def do_clean(number=0):
     """ function to clean the releases """
@@ -11,5 +13,5 @@ def do_clean(number=0):
         keepfiles = 2
     else:
         keepfiles = int(number) + 1
-    local("ls -t versions/ | tail -n +{} | xargs -I _ rm versions/_")
-    run("ls -t /data/web_static/releases/ | tail -n +{} | xargs -I _ rm /data/web_static/releases/_".format(keepfiles))
+    local("ls -t versions/ | tail -n +{} | xargs -I _ rm versions/_".format(keepfiles))
+    run("ls -t /data/web_static/releases/ | tail -n +{} | xargs -I + rm -rf /data/web_static/releases/+".format(keepfiles))
