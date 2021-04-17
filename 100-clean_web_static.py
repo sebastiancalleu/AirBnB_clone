0bfb57204e41144d8685877ebc663ca29bd29b78
sebastian <sebastian.calleu@gmail.com>
2021-04-16 21:43:18 -0500

    fixing pep8

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,5 +13,8 @@ def do_clean(number=0):
         keepfiles = 2
     else:
         keepfiles = int(number) + 1
-    local("ls -t versions/ | tail -n +{} | xargs -I _ rm versions/_".format(keepfiles))
-    run("ls -t /data/web_static/releases/ | tail -n +{} | xargs -I + rm -rf /data/web_static/releases/+".format(keepfiles))
+    local("ls -t versions/ | tail -n +{} | "
+          "xargs -I _ rm versions/_".format(keepfiles))
+    run("ls -t /data/web_static/releases/ | "
+        "tail -n +{} | xargs -I + rm -rf /data/web_static/releases/+"
+        .format(keepfiles))
