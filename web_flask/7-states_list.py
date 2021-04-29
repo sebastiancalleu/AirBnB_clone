5d9753b02650569262e4c74a33c435f1e0114068
sebastian <sebastian.calleu@gmail.com>
2021-04-29 15:59:35 -0500

    adding files for first time.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+""" script that starts a Flask web application """
+from flask import render_template
+from models.state import State
+from models import storage
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/states_list")
+def hello():
+    dct1 = storage.all(State)
+    lt1 = []
+    for i in dct1.values():
+        lt1.append(i)
+    return render_template("7-states_list.html", states=lt1)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
