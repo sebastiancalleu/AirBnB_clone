0bc411510b06b0d587b5f2b9f16fb56bd1559360
sebastian <sebastian.calleu@gmail.com>
2021-04-29 23:20:35 -0500

    adding new files

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,31 @@
+""" script that starts a Flask web application """
+
+from models.amenity import Amenity
+from flask import render_template
+from models.state import State
+from models.city import City
+from models import storage
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/hbnb_filters")
+def hello(text=None):
+    dct1 = storage.all(State)
+    lt1 = []
+    for i in dct1.values():
+        lt1.append(i)
+    dct2 = storage.all(Amenity)
+    lt2 = []
+    for i in dct2.values():
+        lt2.append(i)
+    return render_template("10-hbnb_filters.html", states=lt1, amenities=lt2)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
